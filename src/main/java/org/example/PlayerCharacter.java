5d76611415254d432ff9367a9a04bed6f439de53
louisBerlin <louis.froch@gmail.com>
2024-03-11 12:37:28 +0100

    first commit

--- src/main/java/org/example/PlayerCharacter.java
+++ src/main/java/org/example/PlayerCharacter.java
@@ -18,10 +18,14 @@ public class PlayerCharacter {
 
     public static void move(String s) {
 
-        if(s.equals("w"))       y = y+1;
-        else if(s.equals("s"))  y = y-1;
-        else if(s.equals("a"))  x = x+1;
-        else if(s.equals("d"))  x = x-1;
+        System.out.println("commit");
+
+        switch (s) {
+            case "w" -> y = y + 1;
+            case "s" -> y = y - 1;
+            case "a" -> x = x + 1;
+            case "d" -> x = x - 1;
+        }
 
     }
 
